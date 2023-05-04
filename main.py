e237eca26ab7ddda4580c1a87673ca4b7235821a
TheRosya <hailtherosya@gmail.com>
2023-05-04 22:16:44 +0300

    translate to english

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = int(input("Сколько строк треугольника Паскаля вывести ? "))
-# Формирвание треугольной матрицы t
+# making a triangle matrix of t
 t = [[1]]
 for i in range(1,n):
   if i == 1:
@@ -10,13 +10,13 @@ for i in range(1,n):
       newLine.append(t[i-1][j] + t[i-1][j+1])
     newLine.append(1)
     t.append(newLine)
-print("Треугольная матрица")
+print("Triangle Matrix")
 for i in range(n):
   print(t[i])
       
-# сколько цифр в самом длинном числе
+# how much digits in the biggest number
 nCharLast = max([len(str(x)) for x in t[n-1]])
-print("Вывод сумм под суммируемыми числами:")
+print("Output summed numbers under:")
 for i in range(len(t)):
   indent = ("{:{align}{width}s}".format(" ",align = "^",width=(nCharLast-1))*((len(t)-i)))
   print(indent,end="")
